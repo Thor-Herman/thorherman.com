5d58551d86c3b7a47f421bbb642ef8df4d717c0d
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-06-10 22:03:35 +0200

    Style project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './ProjectCard.css';
 
 const ProjectCard = ({ id, title, description, image }) => {
   return (
     <div className="project-card">
-      <h3 className="title">{title}</h3>
-      <p className="description">{description}</p>
-      <button className="button">Read More</button>
-      <img src={image} alt={'Image of ' + title} />
+      <div className="info">
+        <div className="text">
+          <h3 className="title">{title}</h3>
+          <p className="description">{description}</p>
+        </div>
+        <button className="button">Read More</button>
+      </div>
+      <img src={image} alt={'Image of ' + title} className={title.toLowerCase() + "-image"}/>
     </div>
   );
 };
