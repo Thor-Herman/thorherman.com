a3de6ca80e65ccf55a447f380f2db65120b69921
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-06-10 16:52:18 +0200

    Add shell ScrollArrow component

---
+++ src/components/ScrollArrow.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ReactComponent as ScrollArrowSVG} from '../assets/ScrollArrow.svg';
+
+const ScrollArrow = () => {
+    return <ScrollArrowSVG />
+};
+
+export default ScrollArrow;
