c5c5e99d5226ca94620af0a998d61ce9d4563d1d
Thor-Herman van Eggelen <thorherman.eggelen@gmail.com>
2021-06-13 16:14:28 +0200

    Add scroll-functionality to ScrollArrow

--- src/components/ScrollArrow.js
+++ src/components/ScrollArrow.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { ReactComponent as ScrollArrowSVG } from '../assets/ScrollArrow.svg';
+import { Link } from 'react-scroll';
 
-const ScrollArrow = () => {
-  return <ScrollArrowSVG className={'scroll-arrow'} />;
+const ScrollArrow = ({ elementIdToScrollTo, duration }) => {
+  return (
+    <Link to={elementIdToScrollTo} duration={duration} smooth={true}>
+      <ScrollArrowSVG className={'scroll-arrow'} />
+    </Link>
+  );
 };
 
 export default ScrollArrow;
