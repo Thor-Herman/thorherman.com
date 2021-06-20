8cf783761e56aad855fb71e1ab237297de4c8135
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-06-20 16:09:42 +0200

    Move project cards into own div

--- src/sections/ProjectsSection.js
+++ src/sections/ProjectsSection.js
@@ -4,27 +4,31 @@ import Khabu from '../assets/khabu.png';
 import GalaxyShooter from '../assets/galaxy-shooter.png';
 import ThorHerman from '../assets/thorherman.com.png';
 
+import './ProjectsSection.css';
+
 const ProjectsSection = () => {
   return (
     <section className="projects-section" id="projects-section">
       <p className="section-title">Personal Projects</p>
-      <ProjectCard
-        title="Khabu"
-        description="Real-time multiplayer card game made in React and Spring-Boot."
-        image={Khabu}
-        playableLink={'khabu.eu-north-1.elasticbeanstalk.com/'}
-      />
-      <ProjectCard
-        title="Galaxy Shooter"
-        description="2D Unity project based on a Udemy course"
-        image={GalaxyShooter}
-        playableLink={'http://www.thorherman.com/play.html'}
-      />
-      <ProjectCard
-        title="This Page"
-        description="You're on it right now!"
-        image={ThorHerman}
-      />
+      <div className="projects">
+        <ProjectCard
+          title="Khabu"
+          description="Real-time multiplayer card game made in React and Spring-Boot."
+          image={Khabu}
+          playableLink={'khabu.eu-north-1.elasticbeanstalk.com/'}
+        />
+        <ProjectCard
+          title="Galaxy Shooter"
+          description="2D Unity project based on a Udemy course"
+          image={GalaxyShooter}
+          playableLink={'http://www.thorherman.com/play.html'}
+        />
+        <ProjectCard
+          title="This Page"
+          description="You're on it right now!"
+          image={ThorHerman}
+        />
+      </div>
     </section>
   );
 };
