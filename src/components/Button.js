167d0ffe0ad570f52a60e61ddca0fa02ad106ae5
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-11 21:32:45 +0200

    Remove button padding. Add none type to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = (props) => {
   return (
-    <button className={'button pointer-hover box-shadow ' + props.color}>
+    <button className={`button pointer-hover box-shadow ${props.color} ${props.type}`}>
       <a href={props.link}>{props.text}</a>
     </button>
   );
