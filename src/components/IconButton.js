feef9fb3511f710d441f65c682559e5b6d4b0b11
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-12 22:23:00 +0200

    Add IconButton

---
+++ src/components/IconButton.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './IconButton.css';
+import { ReactComponent as Close } from '../assets/close_white_24dp.svg';
+
+const svgTypes = {
+  close: <Close />,
+};
+
+const IconButton = ({ type, size="small" }) => {
+  const [isBeingHovered, setIsBeingHovered] = useState(false); // :hover property doesn't seem to work with a lower z-index
+  const bgClassName = isBeingHovered ? 'background hovered' : 'background';
+
+  if (type in svgTypes)
+    return (
+      <button
+        onMouseLeave={() => setIsBeingHovered(false)}
+        onMouseEnter={() => setIsBeingHovered(true)}
+        className={"icon-button pointer-hover " + size}
+      >
+        {svgTypes[type]}
+        <div className={bgClassName} />
+      </button>
+    );
+  return null;
+};
+
+export default IconButton;
