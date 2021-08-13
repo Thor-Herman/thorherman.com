2d4ba15c05332c78762057f60ce419ea75f7cc92
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-13 14:41:52 +0200

    Minor style changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = (props) => {
   return (
-    <button className={`button pointer-hover box-shadow ${props.color} ${props.type}`}>
+    <button className={`button pointer-hover box-shadow bold ${props.color} ${props.type}`}>
       <a target="_blank" rel="noreferrer" href={props.link}>{props.text}</a>
     </button>
   );
