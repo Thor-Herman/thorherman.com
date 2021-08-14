a74c3fa195d6ce4c3c06b30700ac6f5e1207c9dc
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-14 18:15:48 +0200

    Add semantic difference to react-scroll Links

--- src/components/NavBarElement.js
+++ src/components/NavBarElement.js
@@ -1,13 +1,13 @@
 import _ from 'lodash';
 import React from 'react';
-import { Link } from 'react-scroll';
+import { Link as ReactScrollLink } from 'react-scroll';
 
 function NavBarElement(props) {
   const isActiveElement = props.sectionName === props.activeSection;
   const className = 'navbar-element roboto ' + (isActiveElement ? 'active' : '');
   return (
     <li className={className}>
-      <Link
+      <ReactScrollLink
         to={props.sectionName.toLowerCase() + '-section'}
         smooth={true}
         onClick={() => {
@@ -16,7 +16,7 @@ function NavBarElement(props) {
         className={"pointer-hover"}
       >
         {_.capitalize(props.sectionName)}
-      </Link>
+      </ReactScrollLink>
     </li>
   );
 }
