23e6485bba2fcc32e0c751119557ae46fff0bde1
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-08-27 20:49:39 +0200

    Create VideoPlayer component

---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,17 @@
+import React, { useRef } from 'react';
+import './VideoPlayer.css'
+
+const VideoPlayer = ({ src }) => {
+  return (
+  <div className="video-player">
+        <iframe
+          title={'Project Video Player'}
+          src={src}
+          frameBorder={0}
+          scrolling="no"
+        />
+      
+  </div>);
+};
+
+export default VideoPlayer;
